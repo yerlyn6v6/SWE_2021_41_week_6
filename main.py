e11d3066f8fac4d06178d7f2b4db4e79b2316925
yerlyn6v6 <yerlyn6v6@g.skku.edu>
2024-04-11 10:12:27 +0000

    init: main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    num_indices = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_indices:
+            return [num_indices[complement], i]
+        num_indices[num] = i
+    return 
